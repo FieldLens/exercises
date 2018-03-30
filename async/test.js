f7ba1649b144e2c3cb8122d1d678611ce1acdf72
Bryan Migliorisi <bryan@fieldlens.com>
2018-03-30 11:28:45 -0400

    update async test

--- async/test.js
+++ async/test.js
@@ -1,99 +1,30 @@
-var assert = require('assert');
-var async = require('./');
+var assert = require("assert");
+var async = require("./");
 
-describe('async', function() {
-
-  describe('has a sequence method that', function() {
-    it('runs functions in sequence', function(done) {
+describe("async", function() {
+  describe("has a sequence method that", function() {
+    it("maps over the functions and returns the correct order", function(done) {
       var fun1 = function(cb) {
-        setTimeout(cb.bind(null, null, 'test'), 10);
+        setTimeout(() => cb(1), 50);
       };
+
       var fun2 = function(cb, data) {
-        setTimeout(cb.bind(null, null, data + 'ing'), 10);
+        setTimeout(() => cb(2), 10);
       };
 
-      // returns a thunk
-      async.sequence([fun1, fun2])(function(err, data) {
-        assert.equal(data, 'testing');
-        done();
-      });
-    });
-
-    it('correctly handles sync functions in sequence', function(done) {
-      var fun1 = function(cb) {
-        cb(null, 'test1');
-      };
-      var fun2 = function(cb, data) {
-          cb(null, data);
+      var fun3 = function(cb, data) {
+        cb(3);
       };
 
-      // returns a thunk
-      async.sequence([fun1, fun2])(function(err, data) {
-        assert.equal(data, 'test1');
-        done();
-      });
-    });
-
-    it('handles delayed thunk invocation', function(done) {
-      var fun1 = function(cb) {
-        cb(null, 'test2');
-      };
-      var fun2 = function(cb, data) {
-        cb(null, data.toUpperCase());
-      };
-
-      // returns a thunk
-      var setter = async.sequence([fun1, fun2])
-
-      setTimeout(function() {
-        setter(function(err, data) {
-          assert.equal(data, 'TEST2');
+      async
+        .map([fun1, fun2, fun3])
+        .then(results => {
+          assert.deepEqual(results, [1, 2, 3]);
           done();
+        })
+        .catch(e => {
+          done(e);
         });
-      }, 100)
     });
   });
-
-  describe('has a parallel method that', function() {
-    it('runs functions in parallel', function(done) {
-      var fun1 = function(cb) {
-        setTimeout(cb.bind(null, null, 'test'), 10);
-      };
-      var fun2 = function(cb) {
-        setTimeout(cb.bind(null, null, 'ing'), 10);
-      };
-
-      // returns a thunk
-      async.parallel([fun1, fun2])(function(err, data) {
-        assert.deepEqual(data, ['test', 'ing']);
-        done();
-      });
-    });
-  });
-
-  describe('has a race method that', function() {
-    it('uses the first completing function', function(done) {
-      var fun1 = function(ms) {
-        return function(cb) {
-          setTimeout(cb.bind(null, null, 'test'), ms);
-        };
-      };
-      var fun2 = function(ms) {
-        return function(cb) {
-          setTimeout(cb.bind(null, null, 'ing'), ms);
-        };
-      };
-
-      // returns a thunk
-      async.race([fun1(10), fun2(20)])(function(err, data) {
-        assert.equal(data, 'test');
-        async.race([fun1(20), fun2(10)])(function(err, data) {
-          assert.equal(data, 'ing');
-          done();
-        });
-
-      });
-    });
-  });
-
 });
