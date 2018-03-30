feaf23bcda0a8027f0028be4ec2af46c1197e8a3
Bryan Migliorisi <bryan@fieldlens.com>
2018-03-30 11:27:13 -0400

    add anagram test

---
+++ anagram/index.js
@@ -0,0 +1,20 @@
+/**
+ * Find Anagrams in Sentence
+ *
+ * Find all of the words that are anagrams with other words in the sentence.
+ *
+ * An anagram is a word, phrase, or name formed by rearranging the letters of another,
+ * such as cinema, formed from iceman.
+ *
+ * Examples:
+ *  spool and pools
+ *  coed and code
+ *
+ * Not an anagram:
+ *  loop and lopp
+ *
+ */
+
+module.exports = sentence => {
+  throw new Error("Not implemented (yet)!");
+};
---
+++ anagram/test.js
@@ -0,0 +1,26 @@
+const assert = require("assert");
+const anagram = require("./index");
+
+describe("Anagram", function() {
+  it("Should find all anagrams in given sentence", function(done) {
+    const sentence = "Mary is in the army and codes with many coeds";
+
+    const results = anagram(sentence);
+
+    assert.equal(results.includes("Mary"), true);
+    assert.equal(results.includes("army"), true);
+    assert.equal(results.includes("codes"), true);
+    assert.equal(results.includes("coeds"), true);
+
+    done();
+  });
+
+  it("Should not find words that have same length and letters, but different number of letters", function(done) {
+    const sentence = "loops llops";
+
+    const results = anagram(sentence);
+    assert.equal(results.length, 0);
+
+    done();
+  });
+});
