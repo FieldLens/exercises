e3fce1472128d6797adc13e099707dff0899e972
Bryan Migliorisi <bryan@migliorisi.com>
2018-06-14 11:20:08 -0400

    Added majority element

---
+++ majority/test.js
@@ -0,0 +1,15 @@
+const assert = require("assert");
+const findMajority = require("./index");
+
+describe("Majority Element", function() {
+  it("Should find the majority element", function(done) {
+    assert.equal(findMajority([1], 1));
+    assert.equal(findMajority([1, 2], null));
+    assert.equal(findMajority([1, 2, 3], null));
+    assert.equal(findMajority([1, 1, 1, 2, 3], 1));
+    assert.equal(findMajority([1, 1, 2, 2, 3], null));
+    assert.equal(findMajority([1, 1, 2, 1, 3, 1, 3], 1));
+
+    done();
+  });
+});
