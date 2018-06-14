e736ed9f47f0a29c4c0ad706be8e341f1e001ef3
Bryan Migliorisi <bryan@migliorisi.com>
2018-06-14 11:00:30 -0400

    Remove implmentation

--- binary-search/index.js
+++ binary-search/index.js
@@ -1,13 +1,3 @@
-function binsearch(ary, elem) {
-  if (!ary || ary.length == 0) { return -1 }
-  if (ary.length === 1 && ary[0] === elem) { return 0 } else { return -1 }
-
-  var middle = ary.length / 2;
-  if (elem < ary[middle]) {
-    return binsearch(ary.splice(0, middle))
-  } else {
-    return binsearch(ary.splice(middle))
-  }
+module.exports = function() {
+  throw new Error("Not implemented (yet)!");
 };
-
-module.exports = binsearch;
