4eb7212fb4acccef65f3e096ac54017192d79db8
Bryan Migliorisi <bryan@fieldlens.com>
2018-07-25 10:22:20 -0400

    Fix assertions

--- majority/test.js
+++ majority/test.js
@@ -3,12 +3,12 @@ const findMajority = require("./index");
 
 describe("Majority Element", function() {
   it("Should find the majority element", function(done) {
-    assert.equal(findMajority([1], 1));
-    assert.equal(findMajority([1, 2], null));
-    assert.equal(findMajority([1, 2, 3], null));
-    assert.equal(findMajority([1, 1, 1, 2, 3], 1));
-    assert.equal(findMajority([1, 1, 2, 2, 3], null));
-    assert.equal(findMajority([1, 1, 2, 1, 3, 1, 3], 1));
+    assert.equal(findMajority([1]), 1);
+    assert.equal(findMajority([1, 2]), null);
+    assert.equal(findMajority([1, 2, 3]), null);
+    assert.equal(findMajority([1, 1, 1, 2, 3]), 1);
+    assert.equal(findMajority([1, 1, 2, 2, 3]), null);
+    assert.equal(findMajority([1, 1, 2, 1, 3, 1, 3]), 1);
 
     done();
   });
